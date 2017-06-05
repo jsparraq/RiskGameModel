e80cc292d4cfc1874963db25771b23898169ad97
jsparraq <jsparraq@unal.edu.co>
2017-06-05 10:19:21 -0500

    Merge origin/master
    Conflicts:
    	src/riskgamemodel/Territory.java

--- src/riskgamemodel/RiskGameModel.java
+++ src/riskgamemodel/RiskGameModel.java
@@ -20,6 +20,32 @@ public class RiskGameModel {
         Player player2 = new Player(false,true,"non-capture","RED",0,0,0,2,0);
         Player player3 = new Player(false,true,"non-capture","GREEN",0,0,0,2,0);
         Player[] players = new Player[]{player1,player2,player3};
+        
+        Territory territory1 = new Territory("OEKRAINË","WHITE",0,false);
+        Territory territory2 = new Territory("IJSLAND","WHITE",0,false);
+        Territory territory3 = new Territory("SCANDINAVIË","WHITE",0,false);
+        Territory territory4 = new Territory("WEST-EUROPA","WHITE",0,false);
+        Territory territory5 = new Territory("GROOT-BRITTANIË","WHITE",0,false);
+        Territory territory6 = new Territory("ZUID-EUROPA","WHITE",0,false);
+        Territory territory7 = new Territory("NOORD-EUROPA","WHITE",0,false);
+        Continent continent1 = new Continent("UIROPA","WHITE",7,new Territory[]{territory1,territory2,territory3,territory4,territory5,territory6,territory7});
+        
+        Territory territory8 = new Territory("VARENIGDE-STATEM(OOST)","WHITE",0,false);
+        Territory territory9 = new Territory("VARENIGDE-STATEM(WEST)","WHITE",0,false);
+        Territory territory10 = new Territory("NORTHWEST-TERRITORIES","WHITE",0,false);
+        Territory territory11 = new Territory("ALBERTA","WHITE",0,false);
+        Territory territory12 = new Territory("MIDDEN-AMERIKA","WHITE",0,false);
+        Territory territory13 = new Territory("ALASKA","WHITE",0,false);
+        Territory territory14 = new Territory("ONTARIO","WHITE",0,false);
+        Territory territory15 = new Territory("QUEBEC","WHITE",0,false);
+        Territory territory16 = new Territory("GROENLAND","WHITE",0,false);
+        Continent continent2 = new Continent("AMEROKI","WHITE",9,new Territory[]{territory8,territory9,territory10,territory11,territory12,territory13,territory14,territory15,territory16});
+        
+        Territory territory3 = new Territory("C","WHITE",0,false);
+        Territory territory4 = new Territory("D","WHITE",0,false);
+        Territory territory5 = new Territory("E","WHITE",0,false);
+        Territory territory6 = new Territory("F","WHITE",0,false);
+        Territory territory7 = new Territory("G","WHITE",0,false);
         Territory territory1 = new Territory("A","WHITE",0,false);
         Territory territory2 = new Territory("B","WHITE",0,false);
         Territory territory3 = new Territory("C","WHITE",0,false);
@@ -27,9 +53,29 @@ public class RiskGameModel {
         Territory territory5 = new Territory("E","WHITE",0,false);
         Territory territory6 = new Territory("F","WHITE",0,false);
         Territory territory7 = new Territory("G","WHITE",0,false);
-        Continent continent1 = new Continent("ContinentA","WHITE",3,new Territory[]{territory1,territory2,territory3});
+        Territory territory1 = new Territory("A","WHITE",0,false);
+        Territory territory2 = new Territory("B","WHITE",0,false);
+        Territory territory3 = new Territory("C","WHITE",0,false);
+        Territory territory4 = new Territory("D","WHITE",0,false);
+        Territory territory5 = new Territory("E","WHITE",0,false);
+        Territory territory6 = new Territory("F","WHITE",0,false);
+        Territory territory7 = new Territory("G","WHITE",0,false);
+        Territory territory1 = new Territory("A","WHITE",0,false);
+        Territory territory2 = new Territory("B","WHITE",0,false);
+        Territory territory3 = new Territory("C","WHITE",0,false);
+        Territory territory4 = new Territory("D","WHITE",0,false);
+        Territory territory5 = new Territory("E","WHITE",0,false);
+        Territory territory6 = new Territory("F","WHITE",0,false);
+        Territory territory7 = new Territory("G","WHITE",0,false);
+        
+        
         Continent continent2 = new Continent("ContinentB","WHITE",2,new Territory[]{territory5,territory4});
         Continent continent3 = new Continent("ContinentC","WHITE",2,new Territory[]{territory6,territory7});
+        Continent continent4 = new Continent("ContinentA","WHITE",3,new Territory[]{territory1,territory2,territory3});
+        Continent continent5 = new Continent("ContinentB","WHITE",2,new Territory[]{territory5,territory4});
+        Continent continent6 = new Continent("ContinentC","WHITE",2,new Territory[]{territory6,territory7});
+       
+        
         Card card1 = new Card("STANDARD","WHITE","Infantry",territory1);
         Card card2 = new Card("STANDARD","WHITE","Cavalry",territory2);
         Card card3 = new Card("STANDARD","WHITE","Artillery",territory3);
